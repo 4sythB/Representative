7c292165ab2d77c2ae9e7436c9daa7542f8f3668
Brad Forsyth <b4syth@gmail.com>
2016-07-11 14:32:27 -0600

    Created model, network controller, and representative controller.

---
+++ Representative/RepresentativeController.swift
@@ -0,0 +1,38 @@
+//
+//  RepresentativeController.swift
+//  Representative
+//
+//  Created by Brad on 7/11/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class RepresentativeController {
+    
+    static let baseURL = NSURL(string: "http://whoismyrepresentative.com/getall_reps_bystate.php")
+    
+    static func searchRepsByState(state: String, completion: ((representative: [Representative]) -> Void)) {
+        guard let url = baseURL else { fatalError("URL optional is nil") }
+        
+        let urlParameters = ["state" : "\(state)"]
+        NetworkController.performRequestForURL(url, httpMethod: .Get, urlParameters: urlParameters) { (data, error) in
+            if let data = data,
+                responseDataString = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                
+                guard let jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: [])) as? [String : AnyObject],
+                    representativesArray = jsonDictionary["results"] as? [[String : AnyObject]] else {
+                        print("Unable to serialize JSON. \n\(responseDataString)")
+                        completion(representative: [])
+                        return
+                }
+                let representatives = representativesArray.flatMap { Representative(dictionary: $0) }
+                completion(representative: representatives)
+                
+            } else {
+                print("No data returned from network request")
+                completion(representative: [])
+            }
+        }
+    }
+}
