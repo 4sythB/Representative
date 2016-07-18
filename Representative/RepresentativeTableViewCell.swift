c052138dfd66c64e01547279e9222febffce5cd7
Brad Forsyth <b4syth@gmail.com>
2016-07-18 14:53:30 -0600

    Created custom table view cell and implemented detail table view methods.

---
+++ Representative/RepresentativeTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  RepresentativeTableViewCell.swift
+//  Representative
+//
+//  Created by Brad on 7/18/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class RepresentativeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var partyLabel: UILabel!
+    @IBOutlet weak var districtLabel: UILabel!
+    @IBOutlet weak var websiteLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateWithRepresentative(representative: Representative) {
+        nameLabel.text = representative.name
+        partyLabel.text = representative.party
+        districtLabel.text = representative.district
+        websiteLabel.text = representative.link
+        phoneLabel.text = representative.phone
+    }
+}
+
+protocol RepresentativeTableViewCellDelegate: class {
+    
+    func updateWithRepresentative(representative: Representative)
+}
